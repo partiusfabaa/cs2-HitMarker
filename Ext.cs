bd0303c0f4885a085f4275399d9be7fa8a17f09e
luxury fabka <96542489+partiusfabaa@users.noreply.github.com>
2024-08-05 18:40:18 +0900

    init

---
+++ Ext.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+
+namespace HitMarker;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct variant_t
+{
+    public nint valuePtr;
+    public fieldtype_t fieldType;
+    public ushort m_flags;
+}
+
+public static class ExtendedFunctions
+{
+    private static readonly MemoryFunctionVoid<nint, string, nint, nint, nint, int> AcceptInputFunc =
+        new(GameData.GetSignature("CEntityInstance_AcceptInput"));
+
+    private static readonly Action<nint, string, nint, nint, nint, int> AcceptInput = AcceptInputFunc.Invoke;
+
+    public static unsafe void FireInput(this CBaseEntity? ent, string input, string param = "",
+        CBaseEntity? activator = null, CBaseEntity? caller = null)
+    {
+        if (ent == null || !ent.IsValid)
+            throw new ArgumentNullException(nameof(ent));
+
+        var strBytes = Encoding.ASCII.GetBytes(param + "\0");
+
+        var secondParam = (variant_t*)Marshal.AllocHGlobal(0xB);
+        var paramStrPtr = Marshal.AllocHGlobal(strBytes.Length);
+
+        secondParam->fieldType = fieldtype_t.FIELD_STRING;
+        secondParam->valuePtr = paramStrPtr;
+
+        Marshal.Copy(strBytes, 0, paramStrPtr, strBytes.Length);
+
+        AcceptInput(ent!.Handle, input, activator?.Handle ?? 0, caller?.Handle ?? 0, (nint)secondParam, 0);
+
+        Marshal.FreeHGlobal(paramStrPtr);
+        Marshal.FreeHGlobal((nint)secondParam);
+    }
+}
---
+++ HitMarker.cs
@@ -0,0 +1,63 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace HitMarker;
+
+public class Config : IBasePluginConfig
+{
+    public string Particle { get; set; } = "particles/upkk/normalshot.vpcf";
+    public int Version { get; set; } = 1;
+}
+
+public class HitMarker : BasePlugin, IPluginConfig<Config>
+{
+    public override string ModuleAuthor => "thesamefabius";
+    public override string ModuleName => "Hit Marker";
+    public override string ModuleVersion => "v1.0.0";
+
+    public Config Config { get; set; } = null!;
+
+    public void OnConfigParsed(Config config)
+    {
+        Config = config;
+    }
+
+    public override void Load(bool hotReload)
+    {
+        RegisterListener<Listeners.OnServerPrecacheResources>(manifest => manifest.AddResource(Config.Particle));
+        RegisterEventHandler<EventPlayerHurt>(EventPlayerHurt);
+    }
+
+    private HookResult EventPlayerHurt(EventPlayerHurt @event, GameEventInfo info)
+    {
+        var attacker = @event.Attacker;
+        if (attacker is null) return HookResult.Continue;
+
+        var player = @event.Userid;
+        if (player == attacker || player?.Team == attacker.Team) 
+            return HookResult.Continue;
+
+        var effectName = Config.Particle;
+        if (string.IsNullOrEmpty(effectName)) 
+            return HookResult.Continue;
+
+        var particle = Utilities.CreateEntityByName<CParticleSystem>("info_particle_system");
+        if (particle == null) 
+            return HookResult.Continue;
+
+        particle.EffectName = effectName;
+        particle.DispatchSpawn();
+
+        Server.NextFrame(() => particle.FireInput("Start", "!activator", attacker.PlayerPawn.Value));
+
+        AddTimer(0.2f, () =>
+        {
+            if (particle.IsValid)
+            {
+                particle.Remove();
+            }
+        });
+
+        return HookResult.Continue;
+    }
+}
